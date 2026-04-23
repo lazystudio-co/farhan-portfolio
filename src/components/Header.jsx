94f31e7244dc263474c0cb9e32daaa9dd835bf64
LazyStudio <lazystudio.org@gmail.com>
2026-04-24 01:10:43 +0600

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -20,7 +20,7 @@ const Header = () => {
           <button
             type="button"
             onClick={() => setIsContactOpen(true)}
-            className="transition-colors hover:text-black cursor-pointer"
+            className=" text-lg font-semibold transition-colors hover:text-black cursor-pointer"
           >
             Contact
           </button>
